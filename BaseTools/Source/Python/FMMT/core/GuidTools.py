573e677652ac42806c22b222caaf469550edd9a1
Jason1 Lin <jason1.lin@intel.com>
2025-03-15 23:35:31 +0800

    BaseTools/FMMT: Fix the Issue Cannot Find the GUID Tool
    - FMMT tool would leverage the "PATH" environment variable to search
      the possible location of GUID tool.
    
    - Executing the sub-process without the "shell=True" argument
      would make the GUID tool could not be found in "PATH" variable.
    
    - This patch added the argument to make the process have the ability
      to get the GUID tool from "PATH" environment variable.
    
    Signed-off-by: Jason1 Lin <jason1.lin@intel.com>

--- BaseTools/Source/Python/FMMT/core/GuidTools.py
+++ BaseTools/Source/Python/FMMT/core/GuidTools.py
@@ -16,7 +16,7 @@ from utils.FmmtLogger import FmmtLogger as logger
 import subprocess
 
 def ExecuteCommand(cmd: list) -> None:
-    subprocess.run(cmd,stdout=subprocess.DEVNULL)
+    subprocess.run(cmd, stdout=subprocess.DEVNULL, shell=True)
 
 class GUIDTool:
     def __init__(self, guid: str, short_name: str, command: str) -> None:
@@ -176,4 +176,3 @@ class GUIDTools:
             raise Exception("Process Failed: is not defined!")
 
 guidtools = GUIDTools()
-
